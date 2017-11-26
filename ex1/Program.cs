8d013a1163a232a0041860a5e2b4be326c7ad8dc
Steven <stevelee2497@gmail.com>
2017-11-27 00:28:47 +0700

    add ex 1

---
+++ ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = StudentGenerator.InitStudents();
+
+            // display student has score >=8 in order of birthday
+            Console.WriteLine("students have score >=8 in order of birthday :");
+            var goodStudents = students.Where(student => student.Score >= 8).OrderBy(student => student.BirthDate);
+            goodStudents.ToList().ForEach(Console.WriteLine);
+
+            // display student has score between 5 and 7.5 in order of name
+            Console.WriteLine("\n\nstudents have score between 5 and 7.5 in order of name");
+            var mediumStudents = students.Where(student => student.Score >= 5 && student.Score <=7.5).OrderBy(student => student.Name);
+            mediumStudents.ToList().ForEach(Console.WriteLine);
+
+            // display student has score between 6 and 8
+            Console.WriteLine("\n\nstudents have score between 6 and 8 in order of name birthday year in (1990-1992)");
+            var nearlyGoodStudents = students.Where(student => student.Score >= 6 && student.Score <= 8 && student.BirthDate.Year >= 1990 && student.BirthDate.Year <= 1992);
+            nearlyGoodStudents.ToList().ForEach(Console.WriteLine);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ ex_1/models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex_1.models
+{
+    public class Student : EasyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+        public string ClassId { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Student()
+        {
+        }
+
+        public Student(int id, string name, double score, string classId, DateTime birthDate)
+        {
+            Id = id;
+            Name = name;
+            Score = score;
+            ClassId = classId;
+            BirthDate = birthDate;
+        }
+    }
+}
